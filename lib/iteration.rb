dc94f964b2f7bb9a8c902a5e0e3aa86687049be9
TeMecha Newton <tanewton0523@gmail.com>
2020-03-17 23:27:22 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,11 +24,14 @@ def find_greater_pair(src)
 end
 
 def total_even_pairs(src)
-    total = 0
+  total = 0
   i = 0
   while i < src.length do
     if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
       total += (src[i][0] + src[i][1])
     end
     i += 1
+  end
+
+  total
 end
